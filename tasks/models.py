043039695debe3760bdb9b79305b9107bebfefda
flavien-hugs <geekactux@gmail.com>
2022-07-07 01:45:18 +0000

    init database

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
+# tasks.models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+    name = models.CharField(max_length=80)
+    slug = models.SlugField(editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    description = models.CharField(max_length=300)
+    collection = models.ForeignKey(
+        to='tasks.Collection',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.collection
