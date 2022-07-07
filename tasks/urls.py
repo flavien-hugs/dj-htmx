fe571c3e95d3115b5c8f96f64313f34a84d344c3
flavien-hugs <geekactux@gmail.com>
2022-07-07 01:59:33 +0000

    edit task views

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,18 @@
 
 from django.urls import path
 
+from tasks import views
 
 app_name='tasks'
 urlpatterns = [
-
+    path(
+        route='',
+        view=views.index_view,
+        name='task_list'
+    ),
+    path(
+        route='create/task/',
+        view=views.create_collection,
+        name='create_task'
+    )
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# tasks.views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    template_name = 'tasks/_list.html'
+    context = {}
+    return render(request, template_name, context)
+
+
+index_view = homePageView
+
+
+def createCollectionView(request):
+    return HttpResponse("")
+
+
+create_collection = createCollectionView
