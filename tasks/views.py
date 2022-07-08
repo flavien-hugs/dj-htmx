b5cfefaac57ee96c613c04ee1c24faba70248175
flavien-hugs <geekactux@gmail.com>
2022-07-08 15:55:19 +0000

    edit atsk views

--- tasks/views.py
+++ tasks/views.py
@@ -2,13 +2,12 @@
 
 from django.http import HttpResponse
 from django.utils.html import escape
-from django.template.loader import render_to_string
 from django.shortcuts import render, redirect, get_object_or_404
 
 from tasks.models import Collection, Task
 
 
-def homePageView(request):
+def homePageView(request, template_name='tasks/_list.html'):
 
     context = {}
 
@@ -19,16 +18,13 @@ def homePageView(request):
         collection = get_object_or_404(Collection, slug=collection_slug)
 
     collections = Collection.objects.order_by('name')
-    context['collections'] = render_to_string(
-        'tasks/partials/_collection.html', context={"collections": collections}
-    )
-
     tasks = collection.task_set.order_by("description")
-    context['tasks'] = render_to_string(
-        'tasks/partials/_task.html', context={"tasks": tasks}
-    )
     
-    template_name = 'tasks/_list.html'
+    context = {
+        'tasks': tasks,
+        'collection': collection,
+        'collections': collections,
+    }
     return render(request, template_name, context=context)
 
 
@@ -43,7 +39,9 @@ def createCollectionView(request):
     if not created:
         return HttpResponse("Cette collection existe déjà !", status=409)
     
-    return HttpResponse(f"<h2>{collection.name}</h2>")
+    context = {'collection': collection}
+    template_name = 'tasks/partials/_collection.html'
+    return render(request, template_name, context)
 
 
 create_collection = createCollectionView
@@ -51,11 +49,14 @@ create_collection = createCollectionView
 
 def createTask(request):
 
-    collection = Collection.get_default_collection()
+    slug = request.POST.get("collection")
+    collection = Collection.objects.get(slug=slug)
     description = escape(request.POST.get('task-description'))
-    Task.objects.create(description=description, collection=collection)
-    
-    return HttpResponse(description)
+    task = Task.objects.create(description=description, collection=collection)
+
+    context = {'task': task}
+    template_name = 'tasks/partials/_task.html'
+    return render(request, template_name, context)
 
 
 create_task = createTask
@@ -65,8 +66,10 @@ def getTasks(request, pk):
     
     collection = get_object_or_404(Collection, pk=pk)
     tasks = collection.task_set.order_by("description")
-    task = [task.description for task in tasks]
-    return HttpResponse("<br>".join(task))
+    
+    context = {"tasks": tasks}
+    template_name = 'tasks/partials/tasks.html'
+    return render(request, template_name, context)
 
 
 get_tasks = getTasks
